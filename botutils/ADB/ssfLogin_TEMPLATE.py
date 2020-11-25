7f2815b59c6aed3891cdad740faff5f0da756d7b
bobbycupp <bcupp@summitps.org>
2020-11-25 12:23:00 -0800

    added template ssfLogin.py

---
+++ botutils/ADB/ssfLogin_TEMPLATE.py
@@ -0,0 +1,13 @@
+#!python3
+'''
+This file is just for saving login details (in clear text)
+To use, update the three member variables below to match
+your login credentials
+Updated token Nov 2020
+
+Find latest details in LastPass.
+'''
+
+user = '[REDACTED]@summitps.org'
+password = '[REDACTED]'
+token = '[REDACTED]'
